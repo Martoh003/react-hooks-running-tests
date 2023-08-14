99f248a4756baccb5ab117c00ded0a67891a7dee
+ <+>
2023-08-14 21:36:45 +0300

    completed assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,14 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import React from "react";
+import Article from "../components/Article";
 
-function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
-}
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
 
+    // Debug DOM structure
+    screen.debug();
+
+    expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
 export default Article;
